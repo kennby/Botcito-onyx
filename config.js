e7e1c6cb9762096507a302692255fdb023c3f337
kennby <153956421+kennby@users.noreply.github.com>
2023-12-28 00:59:01 -0500

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ import { watchFile, unwatchFile } from 'fs'
 import chalk from 'chalk'
 import { fileURLToPath, pathToFileURL } from 'url'
 
-global.owner = [['59177601773', 'Alba070503', true]]
+global.owner = [['51946512638', 'kennn', true]]
 
 //BETA: Si quiere evitar escribir el número que será bot en la consola, agregué desde aquí entonces:
 //Sólo aplica para opción 2 (ser bot con código de texto de 8 digitos)
